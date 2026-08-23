e2e8055374a1ee68ebe72cb592bb7e9ef7919851
Phenyl316 <jeremy.croisille.it@gmail.com>
2026-08-23 03:41:14 +0200

    Update jetpack.sh

--- jetpack.sh
+++ jetpack.sh
@@ -332,7 +332,10 @@ while true; do
 
                                         "Fully replace the file")
                                             read -p "What do you want to write into this file ? : " REPLACEFILE
-                                            "$choice" -c 'import os; os.setuid(0);open("'$FILE'","w+").write("\n'"$REPLACEFILE"'")'
+                                            
+                                            OLD_PERMS=$(stat -c "%a" "$FILE")
+                                            
+                                            "$choice" -c 'import os; os.setuid(0); os.system("chmod 777 '"$FILE"'"); open("'"$FILE"'","w+").write("\n'"$REPLACEFILE"'"); os.system("chmod '"$OLD_PERMS"' '"$FILE"'")'
 
                                             echo "contents of the file after modification : "
                                             echo " "
@@ -344,7 +347,10 @@ while true; do
                             
                                         "Append data to the file")
                                             read -p "What do you want to append this file ? : " APPENDFILE
-                                            "$choice" -c 'import os; os.setuid(0);open("'$FILE'","a").write("\n'"$APPENDFILE"'")'
+
+                                            OLD_PERMS=$(stat -c "%a" "$FILE")
+                                            
+                                            "$choice" -c 'import os; os.setuid(0); os.system("chmod 777 '"$FILE"'"); open("'"$FILE"'","w+").write("\n'"$APPENDFILE"'"); os.system("chmod '"$OLD_PERMS"' '"$FILE"'")'
 
                                             echo "contents of the file after modification : "
                                             echo " "
